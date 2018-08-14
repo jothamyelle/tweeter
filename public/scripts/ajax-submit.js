fae44a568abac901a915ce220ea83aa8dbc755ba
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-14 23:49:18 +0000

    Implemented loadTweets function - Now using AJAX to get tweets from server

--- public/scripts/ajax-submit.js
+++ public/scripts/ajax-submit.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-  function go() {
-    $("form").on("submit", function(event) {
-      event.preventDefault();
-      var input = $("form").serialize();
-    
-      $.post("/tweets", input)
-      .done(function(data) {
-        var newTweet = createTweetElement(data);
-        $("#tweet-container").append(newTweet);
-        $("#textInput").val('');
-      });
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    var input = $("form").serialize();
+  
+    $.post("/tweets", input)
+    .done(function(data) {
+      var newTweet = createTweetElement(data);
+      $("#tweet-container").append(newTweet);
+      $("#textInput").val('');
     });
-  }
-  go();
+  });
 });
