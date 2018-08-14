2ad8d03c0bf6e97ef948d76d3179adecab719ac6
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-14 01:13:14 +0000

    super minor: changed spacing in js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('textarea').keyup(function() {
     let count = 140 - (this.value.length);
-    let parent = $(this).parent().get( 0 ); 
+    let parent = $(this).parent().get(0); 
     let counter = $(parent).children(".counter");
     if (count <= 0) {
       counter.css("color","red");
