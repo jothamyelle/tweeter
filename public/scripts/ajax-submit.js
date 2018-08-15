10766652592080995482a3de46491740101f43be
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-15 03:27:14 +0000

    Adjusted validation error display to slidedown

--- public/scripts/ajax-submit.js
+++ public/scripts/ajax-submit.js
@@ -8,18 +8,18 @@ $(document).ready(function() {
   $("form").on("submit", function(event) {
     event.preventDefault();
     if ($("#textInput").val() === "") {
-      $("#errorMessage").text("Sorry, you can't submit an empty tweet!").css("display","block");
+      $("#errorMessage").text("Sorry, you can't submit an empty tweet!").slideDown().css("display","block");
       return false;
     }
     if ($("#textInput").val().length > 140) {
-      $("#errorMessage").text("Sorry, you can't submit a tweet that's longer than 140 characters!").css("display","block");
+      $("#errorMessage").text("Sorry, you can't submit a tweet that's longer than 140 characters!").slideDown().css("display","block");
       return false;
     }
 
     var input = escape($("#textInput").val());
 
     if (input.indexOf("&lt;") >= 0 || input.indexOf("&gt;") >= 0) {
-      $("#errorMessage").text(`Sorry, your post can't contain these characters: < >`).css("display","block");
+      $("#errorMessage").text(`Sorry, your post can't contain these characters: < >`).slideDown().css("display","block");
       return false;
     }
 
